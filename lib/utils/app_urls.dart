ce8a0d014ef3767eae9397eac721ac80188301a7
muhammad-ahsan-kz <muhammad.ahsan@skynetsilicon.com>
2025-12-26 12:33:57 +0500

    Added base url in env and bash file

--- lib/utils/app_urls.dart
+++ lib/utils/app_urls.dart
@@ -7,7 +7,8 @@ class AppUrls {
   // static const String baseUrl =
   //     "http://192.168.100.99:4000/api/"; // For Mobile Testing
 
-       static const String baseUrl = String.fromEnvironment(
+//
+  static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
     defaultValue: 'http://192.168.100.99:4000/api/',
   );
